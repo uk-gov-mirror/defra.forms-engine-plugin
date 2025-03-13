3934a2160bfbc15e5f8b1de90306637b631c9553
David Stone <davidjamesstone@gmail.com>
2025-03-13 10:58:23 +0000

    Export nunjucks filters and globals for use in consumers

--- src/server/plugins/engine/index.ts
+++ src/server/plugins/engine/index.ts
@@ -1,7 +1,23 @@
 import { plugin } from '~/src/server/plugins/engine/plugin.js'
+import {
+  checkComponentTemplates,
+  checkErrorTemplates,
+  evaluate
+} from '~/src/server/plugins/nunjucks/environment.js'
+import * as filters from '~/src/server/plugins/nunjucks/filters/index.js'
 
 export { getPageHref } from '~/src/server/plugins/engine/helpers.js'
 export { configureEnginePlugin } from '~/src/server/plugins/engine/configureEnginePlugin.js'
 export { CacheService } from '~/src/server/services/index.js'
 
+export { context } from '~/src/server/plugins/nunjucks/context.js'
+
+const globals = {
+  checkComponentTemplates,
+  checkErrorTemplates,
+  evaluate
+}
+
+export { filters }
+export { globals }
 export default plugin
