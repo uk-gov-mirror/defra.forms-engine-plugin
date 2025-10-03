2cb0fbaf6eea256595ed61127bcba7c03fc303da
Alex Luckett <alexgluckett@gmail.com>
2025-10-03 17:12:25 +0100

    Remove unneccessary layer

--- src/server/plugins/forms/index.ts
+++ src/server/plugins/forms/index.ts
@@ -2,7 +2,6 @@ import { type Plugin, type Server } from '@hapi/hapi'
 
 import enginePlugin from '~/src/server/plugins/engine/index.js'
 import { type PluginOptions } from '~/src/server/plugins/forms/types.js'
-import { plugin as manifestPlugin } from '~/src/server/plugins/manifest/index.js'
 
 export const plugin = {
   name: '@defra/forms-index',
@@ -10,9 +9,16 @@ export const plugin = {
   multiple: true,
   async register(server: Server, options?: PluginOptions) {
     // routes that should have a predictable URL
-    await server.register({
-      plugin: manifestPlugin,
-      options
+    server.route({
+      method: 'GET',
+      path: '/forms-manifest',
+      handler: (_, h) => {
+        const prefix = options?.serverRegistrationOptions?.routes?.prefix ?? ''
+
+        return h.response({
+          engineBaseUrl: prefix
+        })
+      }
     })
 
     // forms can be prefixed to prevent collisions
