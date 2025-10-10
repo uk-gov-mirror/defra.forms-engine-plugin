fe12948202386e5d4f6deb3874f7feb5b8162b2c
David Stone <davidjamesstone@gmail.com>
2025-10-10 10:04:30 +0100

    Handle OS service exceptions

--- src/server/plugins/postcode-lookup/service.js
+++ src/server/plugins/postcode-lookup/service.js
@@ -1,4 +1,5 @@
 import { getErrorMessage } from '@defra/forms-model'
+import Boom from '@hapi/boom'
 
 import { createLogger } from '~/src/server/common/helpers/logging/logger.js'
 import { getJson } from '~/src/server/services/httpService.js'
@@ -6,36 +7,63 @@ import { getJson } from '~/src/server/services/httpService.js'
 const logger = createLogger()
 
 /**
+ * Returns an empty result set
+ */
+function empty() {
+  return []
+}
+
+/**
+ * Logs OS places errors
+ * @param {unknown} err - the error
+ * @param {string} endpoint - the OS api endpoint
+ */
+function logErrorAndReturnEmpty(err, endpoint) {
+  const msg = `${getErrorMessage(err)} ${(Boom.isBoom(err) && err.data?.payload?.error?.message) ?? ''}`
+
+  logger.error(err, `Exception occured calling OS places ${endpoint} - ${msg}}`)
+
+  return empty()
+}
+
+/**
+ * Fetch data from OS API
+ * @param {string} url - the url to get address json data from
+ * @param {string} endpoint - the url endpoint description for logging
+ */
+async function getAddressData(url, endpoint) {
+  const getJsonByType =
+    /** @type {typeof getJson<DeliveryPointAddressResult>} */ (getJson)
+
+  try {
+    const response = await getJsonByType(url)
+
+    if (response.error) {
+      return logErrorAndReturnEmpty(response.error, endpoint)
+    }
+
+    const results = response.payload.results
+
+    if (!Array.isArray(results)) {
+      return empty()
+    }
+
+    return results.map((result) => formatAddress(result.DPA))
+  } catch (err) {
+    return logErrorAndReturnEmpty(err, endpoint)
+  }
+}
+
+/**
  * OS places search
  * @param {string} query - the search term
  * @param {string} apiKey - the OS api key
  */
 export async function searchByQuery(query, apiKey) {
-  const getJsonByType =
-    /** @type {typeof getJson<DeliveryPointAddressResult>} */ (getJson)
+  const endpoint = 'find'
+  const url = `https://api.os.uk/search/places/v1/${endpoint}?query=${encodeURIComponent(query)}&key=${apiKey}`
 
-  const url = `https://api.os.uk/search/places/v1/find?query=${encodeURIComponent(query)}&key=${apiKey}`
-
-  const response = await getJsonByType(url)
-
-  if (response.error) {
-    const error = response.error
-
-    logger.error(
-      error,
-      `Exception occured calling OS places find ${getErrorMessage(error)}`
-    )
-
-    return []
-  }
-
-  const results = response.payload.results
-
-  if (!Array.isArray(results)) {
-    return []
-  }
-
-  return results.map((result) => formatAddress(result.DPA))
+  return getAddressData(url, endpoint)
 }
 
 /**
@@ -44,31 +72,10 @@ export async function searchByQuery(query, apiKey) {
  * @param {string} apiKey - the OS api key
  */
 export async function searchByPostcode(postcode, apiKey) {
-  const getJsonByType =
-    /** @type {typeof getJson<DeliveryPointAddressResult>} */ (getJson)
+  const endpoint = 'postcode'
+  const url = `https://api.os.uk/search/places/v1/${endpoint}?postcode=${encodeURIComponent(postcode.replace(/\s/g, ''))}&key=${apiKey}`
 
-  const url = `https://api.os.uk/search/places/v1/postcode?postcode=${encodeURIComponent(postcode.replace(/\s/g, ''))}&key=${apiKey}`
-
-  const response = await getJsonByType(url)
-
-  if (response.error) {
-    const error = response.error
-
-    logger.error(
-      error,
-      `Exception occured calling OS places postcode ${getErrorMessage(error)}`
-    )
-
-    return []
-  }
-
-  const results = response.payload.results
-
-  if (!Array.isArray(results)) {
-    return []
-  }
-
-  return results.map((result) => formatAddress(result.DPA))
+  return getAddressData(url, endpoint)
 }
 
 /**
@@ -77,31 +84,10 @@ export async function searchByPostcode(postcode, apiKey) {
  * @param {string} apiKey - the OS api key
  */
 export async function searchByUPRN(uprn, apiKey) {
-  const getJsonByType =
-    /** @type {typeof getJson<DeliveryPointAddressResult>} */ (getJson)
+  const endpoint = 'uprn'
+  const url = `https://api.os.uk/search/places/v1/${endpoint}?uprn=${uprn}&key=${apiKey}`
 
-  const url = `https://api.os.uk/search/places/v1/uprn?uprn=${uprn}&key=${apiKey}`
-
-  const response = await getJsonByType(url)
-
-  if (response.error) {
-    const error = response.error
-
-    logger.error(
-      error,
-      `Exception occured calling OS places UPRN ${getErrorMessage(error)}`
-    )
-
-    return []
-  }
-
-  const results = response.payload.results
-
-  if (!Array.isArray(results)) {
-    return []
-  }
-
-  return results.map((result) => formatAddress(result.DPA))
+  return getAddressData(url, endpoint)
 }
 
 /**
