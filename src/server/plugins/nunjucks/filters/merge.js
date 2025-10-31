acb2c68a448ba739a8c7f64ccee9a17896288137
Jez Barnsley <jbarnsley.github@gmail.com>
2025-10-31 12:13:03 +0000

    Sonar fix

--- src/server/plugins/nunjucks/filters/merge.js
+++ src/server/plugins/nunjucks/filters/merge.js
@@ -2,6 +2,7 @@
  * Nunjucks filter to get the page for a given path
  * @param {Record<string, any> | string} targetDictionary - Object to extend
  * @param {Record<string, any> | string} sourceDictionary - Object to merge into target
+ * @returns {Record<string, any> | string}
  */
 export function merge(targetDictionary, sourceDictionary) {
   if (
